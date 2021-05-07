2a9238a12fd538e254521afbff93aa0d8de6c1ea
Shinde Jyotiram <shindejyotiram07@gmail.com>
2021-05-07 17:07:59 +0530

    2

--- LogiliteAssignment2/src/JavaBasicProgram/FinalKewordDemo.java
+++ LogiliteAssignment2/src/JavaBasicProgram/FinalKewordDemo.java
@@ -1,9 +1,20 @@
 package JavaBasicProgram;
 
 public class FinalKewordDemo {
+	
+	final int speedlimit=90;//final variable  
+	  int a=10;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		FinalKewordDemo fd=new FinalKewordDemo();
+		fd.a=20;
+		//We can not change the final variable value
+		//fd.speedlimit=110;
+		System.out.println(fd.speedlimit);
+		System.out.println(fd.a);
+		
+		  
 
 	}
 
